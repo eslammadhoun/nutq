722db7a0774616d7a01e0e19c1749df85c23fc17
Eslam Madhoun <eslammadhoun3@gmail.com>
2026-08-11 02:36:32 +0300

    refactor(auth): remove unused authentication feature components

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,11 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:nutq/core/network/dio_factory.dart';
 import 'package:nutq/core/preferences/app_preferences.dart';
-import 'package:nutq/features/auth/data/datasources/auth_api_service.dart';
-import 'package:nutq/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:nutq/features/auth/domain/repositories/auth_repository.dart';
-import 'package:nutq/features/auth/presentation/cubit/auth_cubit.dart';
 
 final sl = GetIt.instance;
 
@@ -13,14 +8,4 @@ Future<void> setupDI() async {
   final prefs = await SharedPreferences.getInstance();
 
   sl.registerLazySingleton<AppPreferences>(() => AppPreferences(prefs));
-
-  sl.registerLazySingleton<AuthApiService>(
-    () => AuthApiService(DioFactory.instance),
-  );
-
-  sl.registerLazySingleton<AuthRepository>(
-    () => AuthRepositoryImpl(sl<AuthApiService>(), sl<AppPreferences>()),
-  );
-
-  sl.registerFactory<AuthCubit>(() => AuthCubit(sl<AuthRepository>()));
 }
